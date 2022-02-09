a7555f443835d9ff0e608e4cb136a37919af80a1
ANFO <artem29040506000@gmail.com>
2022-02-09 16:44:31 +0200

    add splash screen

---
+++ app/src/main/java/com/example/symposium/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.symposium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.symposium.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
