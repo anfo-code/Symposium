48e7a2ddc8bd75978ad944fddee56f948d0c9f7c
ANFO <artem29040506000@gmail.com>
2022-02-17 17:21:52 +0200

    add log in functionality

--- app/src/main/java/com/example/symposium/activities/SignInActivity.kt
+++ app/src/main/java/com/example/symposium/activities/SignInActivity.kt
@@ -1,8 +1,11 @@
 package com.example.symposium.activities
 
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
@@ -12,18 +15,23 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.symposium.R
 import com.example.symposium.databinding.SignInActivityBinding
 import com.example.symposium.utils.Helper
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignInActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: SignInActivityBinding
     private val helper = Helper()
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = SignInActivityBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         setToolbar()
 
@@ -36,11 +44,45 @@ class SignInActivity : BaseActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.buttonSignIn -> {
-                Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+                signIn()
                 helper.hideKeyboard(this)
             }
             R.id.constraintLayoutSignIn -> {
                 helper.hideKeyboard(this)
+            }
+        }
+    }
+
+    private fun signIn() {
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+        if (validateForm(email, password)) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        showErrorSnackBar("Oops! Something went wrong! Please, try again!")
+                        Log.e("Error", task.exception!!.message.toString())
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                Log.i("ELSE BLOCK", "ACTIVATED")
+                true
             }
         }
     }
